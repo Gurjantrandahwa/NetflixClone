1fb86c3364d9c318844f1b3d0ecd1166913ed01f
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-10-03 12:06:22 +0530

    netflix-clone

---
+++ src/Common/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ZHrw5gcGBwhNNKFI6FnXIOIVCtLpkR8",
+    authDomain: "netflix-clone-4e2cc.firebaseapp.com",
+    projectId: "netflix-clone-4e2cc",
+    storageBucket: "netflix-clone-4e2cc.appspot.com",
+    messagingSenderId: "235518870980",
+    appId: "1:235518870980:web:c00cd074e6517287971091",
+    measurementId: "G-5211DDG17M"
+};
---
+++ src/Components/HomeScreen/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HomeScreen.css";
+import Navbar from "../Navbar/Navbar";
+export default function HomeScreen() {
+    return <div className={"home-screen"}>
+        <Navbar/>
+
+    </div>
+}
